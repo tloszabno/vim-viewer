35228bee18e7caa1dbc9c33999c250c822e82bb5
Tomasz Los <tloszabno@gmail.com>
2020-05-25 22:07:01 +0200

    Vim browser

--- main.js
+++ main.js
@@ -1,15 +1,46 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu, MenuItem } = require("electron");
 
-function createWindow () {
+function createWindow() {
   let win = new BrowserWindow({
     width: 1024,
     height: 768,
     webPreferences: {
-      nodeIntegration: true
-    }
-  })
+      nodeIntegration: true,
+    },
+  });
 
-  win.loadFile('index.html')
+  win.loadFile("index.html");
+
+  /// MENU + IPC
+  const menu = createMenu(win);
+  Menu.setApplicationMenu(menu);
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
+
+const createMenu = (win) => {
+  const menu = new Menu();
+  menu.append(
+    new MenuItem({
+      label: "next",
+      accelerator: "k",
+      click: () => win.webContents.send("img-next"),
+    })
+  );
+  menu.append(
+    new MenuItem({
+      label: "prev",
+      accelerator: "j",
+      click: () => win.webContents.send("img-prev"),
+    })
+  );
+  menu.append(
+    new MenuItem({
+      label: "Toggle &Developer Tools",
+      accelerator: "Shift+Ctrl+I",
+      click: () => win.webContents.toggleDevTools(),
+    })
+  );
+  win.setMenuBarVisibility(false)
+  return menu;
+};
--- render.js
+++ render.js
@@ -1,33 +1,58 @@
 const fs = require("fs");
 const path = require("path");
+const { ipcRenderer } = require("electron");
 
 const galleryPath = "/home/tomek/Pictures";
 let currentFileName = null;
 
 window.addEventListener("DOMContentLoaded", () => {
-  const files = fs
-    .readdirSync(galleryPath)
-    .filter((item) => item.endsWith(".jpg"));
+  const files = readImagesInDir();
   if (files.length == 0) {
     return;
   }
 
   currentFileName = files[0];
+  paintImage();
+});
 
+ipcRenderer.on("img-next", () => {
+  changeCurrentImage("next");
+});
+ipcRenderer.on("img-prev", () => {
+  changeCurrentImage("prev");
+});
+
+const paintImage = () => {
+  console.log("painting " + currentFileName);
   const fileAsBase64 = fs.readFileSync(
     path.join(galleryPath, currentFileName),
     { encoding: "base64" }
   );
   document.getElementById("imagePreview").src =
     "data:image/png;base64," + fileAsBase64;
+};
 
-  files.forEach((item) => {
-    const li = document.createElement("li");
-    li.innerText = item;
-    document.getElementById("fs-list").appendChild(li);
-  });
-});
+const changeCurrentImage = (direction) => {
+  const images = readImagesInDir();
+  let idx = images.findIndex((img) => img == currentFileName);
+  console.log({idx, direction, images})
+  if (images.length == 0) {
+    return;
+  }
 
-// prosta przeglądarka do zdjęć
-// jeden skrót na dalej / wcześniej,
-// odczytanie zdjęcia z dysku
+  idx = direction === "prev" ? idx - 1 : idx + 1;
+
+  if (idx >= images.length) {
+    idx = 0;
+  }
+  if (idx < 0) {
+    idx = images.length - 1;
+  }
+
+  currentFileName = images[idx];
+  paintImage();
+};
+
+const readImagesInDir = () => {
+  return fs.readdirSync(galleryPath).filter((item) => item.endsWith(".jpg"));
+};
