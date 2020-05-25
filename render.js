ec80e29202daceaa29654dd8feccb5d1b08916e5
Tomasz Los <tloszabno@gmail.com>
2020-05-25 21:44:04 +0200

    show image

--- render.js
+++ render.js
@@ -1,18 +1,32 @@
 const fs = require("fs");
+const path = require("path");
 
-const path = "/home/tomek";
-
+const galleryPath = "/home/tomek/Pictures";
+let currentFileName = null;
 
 window.addEventListener("DOMContentLoaded", () => {
-    fs.readdir(path, (err, items) => {
-        items.forEach(item => {
-            const li = document.createElement("li")
-            li.innerText = item
-            document.getElementById("fs-list").appendChild(li)
-        })
-    });
-});
+  const files = fs
+    .readdirSync(galleryPath)
+    .filter((item) => item.endsWith(".jpg"));
+  if (files.length == 0) {
+    return;
+  }
 
+  currentFileName = files[0];
+
+  const fileAsBase64 = fs.readFileSync(
+    path.join(galleryPath, currentFileName),
+    { encoding: "base64" }
+  );
+  document.getElementById("imagePreview").src =
+    "data:image/png;base64," + fileAsBase64;
+
+  files.forEach((item) => {
+    const li = document.createElement("li");
+    li.innerText = item;
+    document.getElementById("fs-list").appendChild(li);
+  });
+});
 
 // prosta przeglądarka do zdjęć
 // jeden skrót na dalej / wcześniej,
